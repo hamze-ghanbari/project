eb7c8c9c27a3fea5e7b7f413e286a32a493e1892
hamze ghanbari <infogh1000@gmail.com>
2021-10-06 15:12:03 +0330

    edit

--- src/share/app.module.ts
+++ src/share/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from 'src/app/app.component';
-import { MenuComponent } from './menu/menu.component';
-
+ 
+ 
+ import { MenuComponent } from 'src/share/menu/menu.component';
 @NgModule({
   declarations: [
-  
+   
      MenuComponent
   
     
